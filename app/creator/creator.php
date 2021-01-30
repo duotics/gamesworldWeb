ad57a6a487458357cffaab15bb43cbfa2ca31a02
¨lptbig¨ <kd.banegas@gmail.com>
2021-01-30 14:29:19 -0500

    clean styles

--- app/creator/creator.php
+++ app/creator/creator.php
@@ -5,25 +5,6 @@ include(RAIZf.'nav.php'); ?>
 <style>
 .game-head{
     background-image: url(<?php echo $RAIZd."gamesbg/".$row['bg'] ?>); 
-    /* Background image is centered vertically and horizontally at all times */
-    background-position: center center;
-    /* Background image doesn't tile */
-    background-repeat: no-repeat;
-    /* Background image is fixed in the viewport so that it doesn't move when the content's height is greater than the image's height */
-    background-attachment: fixed;
-    /* This is what makes the background image rescale based on the container's size */
-    background-size: cover;
-    /* Set a background color that will be displayed while the background image is loading */
-    background-color: #eee;
-}
-.cont-streamer{
-    overflow:hidden;
-    background:#eee;
-}
-.img-streamer{
-    height:100%;
-    width:100%;
-    object-fit: cover;
 }
 </style>
 <div class="container">
@@ -35,7 +16,6 @@ include(RAIZf.'nav.php'); ?>
             Filtros
         </div>
     </div>
-
     <div class="bg-white">
         <div class="container p-4">
             <div class="row row-cols-1 row-cols-sm-3 row-cols-md-4 row-cols-lg-6 g-4">
